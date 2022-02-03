bf972a99746c729f165c97898e8826cf3c713bb0
Benjihs2002 <cb201203@etu.univ-lehavre.fr>
2022-02-03 18:03:29 +0100

    Reordering

---
+++ Courses/Cryptographie/Basics/fromHex.py
@@ -0,0 +1,7 @@
+# admit you have a string encrypt by hex code
+# you can convert her by the function bytes.fromhex
+# the invert of this function is hex function as you can see below
+
+res = 'well played'
+var = bytes.hex(res)
+print(bytes.fromhex('63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d'))
