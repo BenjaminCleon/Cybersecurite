af662726d26a0fd0809f797bd0cf0635e87b9bd4
Benjihs2002 <cb201203@etu.univ-lehavre.fr>
2022-02-25 10:37:46 +0100

    End of odular

---
+++ Courses/Cryptographie/Modular/ChineseRemainderTheorem.py
@@ -0,0 +1,10 @@
+'''
+The Chinese Remainder Theorem gives a unique solution to a set of linear congruences if their moduli are coprime.
+
+This means, that given a set of arbitrary integers ai, and pairwise coprime integers ni, such that the following linear congruences hold:
+
+x ≡ a1 mod n1
+x ≡ a2 mod n2
+...
+x ≡ an mod nn
+'''
